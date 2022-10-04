f8a01c8d7a7c3579cc3182cb2d0c544a080cdf3b
JOSH-HOME\Jphines90 <joshua.paul.hines@gmail.com>
2022-10-03 19:53:43 -0700

    Includes two solutions for solving the prompt

--- multiples3or5.py
+++ multiples3or5.py
@@ -4,3 +4,18 @@
 #Prompt:
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+def test_1():
+    sum = 0
+    for number in range(1,1001):
+        if number % 3 == 0 or number % 5 == 0:
+            sum += number
+    return sum
+
+print(test_1())
+
+def test_2():
+    return sum([x for x in range(1,1001) if x % 3 == 0 or x % 5 == 0])
+
+print(test_2())
